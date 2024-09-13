461fcdfef48be1ef2bdb80f8b54db877e6ca0b16
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-09-13 15:43:38 -0500

    feat: add info about team members

--- tests/Welcome.test.tsx
+++ tests/Welcome.test.tsx
@@ -4,40 +4,41 @@ import { MemoryRouter } from "react-router-dom";
 import Welcome from "../app/components/welcome/Welcome";
 
 vi.mock("react-i18next", () => ({
-  useTranslation: () => ({
-    t: (key: string) => key,
-  }),
+    useTranslation: () => ({
+        t: (key: string) => key,
+        i18n: { language: "en" },
+    }),
 }));
 
 describe("Welcome Component", () => {
-  it("renders the welcome title", () => {
-    render(
-        <MemoryRouter>
-          <Welcome />
-        </MemoryRouter>
-    );
-    expect(screen.getByText("welcome.reactGroup")).toBeInTheDocument();
-  });
+    it("renders the welcome title", () => {
+        render(
+            <MemoryRouter>
+                <Welcome />
+            </MemoryRouter>
+        );
+        expect(screen.getByText("welcome.reactGroup")).toBeInTheDocument();
+    });
 
-  it("renders team members", () => {
-    render(
-        <MemoryRouter>
-          <Welcome />
-        </MemoryRouter>
-    );
-    expect(screen.getByText("welcome.maksimName")).toBeInTheDocument();
-    expect(screen.getByText("welcome.egorName")).toBeInTheDocument();
-    expect(screen.getByText("welcome.dmitryName")).toBeInTheDocument();
-  });
+    it("renders team members", () => {
+        render(
+            <MemoryRouter>
+                <Welcome />
+            </MemoryRouter>
+        );
+        expect(screen.getByText("welcome.maksimName")).toBeInTheDocument();
+        expect(screen.getByText("welcome.egorName")).toBeInTheDocument();
+        expect(screen.getByText("welcome.dmitryName")).toBeInTheDocument();
+    });
 
-  it("renders GitHub profile buttons", () => {
-    render(
-        <MemoryRouter>
-          <Welcome />
-        </MemoryRouter>
-    );
-    expect(
-        screen.getAllByRole("button", { name: "welcome.githubProfile" })
-    ).toHaveLength(3);
-  });
+    it("renders GitHub profile buttons", () => {
+        render(
+            <MemoryRouter>
+                <Welcome />
+            </MemoryRouter>
+        );
+        expect(
+            screen.getAllByRole("button", { name: "welcome.githubProfile" })
+        ).toHaveLength(3);
+    });
 });
